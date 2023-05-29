69ca676458a309336aa9fe93cf65497b99f2228d
sarthak-io <sarthak25ic049@satiengg.in>
2023-05-29 12:18:55 +0530

    updated

--- Experience/World/Environment.js
+++ Experience/World/Environment.js
@@ -27,48 +27,48 @@ export default class Environment {
    if(this.experience.sizes.device === "mobile"){
     this.sunLight.shadow.mapSize.set(0,0);
     this.sunLight.shadow.normalBias=0;
-    this.sunLight.intensity = 1.5; 
-    this.ambientLight.intensity = 0.5; 
+    this.sunLight.intensity = 0.5; 
+    this.ambientLight.intensity = 0; 
    }
    }
    
    switchTheme(theme) {
-    // console.log(this.sunLight);
-    if (theme === "dark") {
-        GSAP.to(this.sunLight.color, {
-            r: 0.17254901960784313,
-            g: 0.23137254901960785,
-            b: 0.6862745098039216,
-        });
-        GSAP.to(this.ambientLight.color, {
-            r: 0.17254901960784313,
-            g: 0.23137254901960785,
-            b: 0.6862745098039216,
-        });
-        GSAP.to(this.sunLight, {
-            intensity: 0.78,
-        });
-        GSAP.to(this.ambientLight, {
-            intensity: 0.78,
-        });
-    } else {
-        GSAP.to(this.sunLight.color, {
-            r: 255 / 255,
-            g: 255 / 255,
-            b: 255 / 255,
-        });
-        GSAP.to(this.ambientLight.color, {
-            r: 255 / 255,
-            g: 255 / 255,
-            b: 255 / 255,
-        });
-        GSAP.to(this.sunLight, {
-            intensity: 3,
-        });
-        GSAP.to(this.ambientLight, {
-            intensity: 1,
-        });
-    }
+    // // console.log(this.sunLight);
+    // if (theme === "dark") {
+    //     GSAP.to(this.sunLight.color, {
+    //         r: 0.17254901960784313,
+    //         g: 0.23137254901960785,
+    //         b: 0.6862745098039216,
+    //     });
+    //     GSAP.to(this.ambientLight.color, {
+    //         r: 0.17254901960784313,
+    //         g: 0.23137254901960785,
+    //         b: 0.6862745098039216,
+    //     });
+    //     GSAP.to(this.sunLight, {
+    //         intensity: 0.78,
+    //     });
+    //     GSAP.to(this.ambientLight, {
+    //         intensity: 0.78,
+    //     });
+    // } else {
+    //     GSAP.to(this.sunLight.color, {
+    //         r: 255 / 255,
+    //         g: 255 / 255,
+    //         b: 255 / 255,
+    //     });
+    //     GSAP.to(this.ambientLight.color, {
+    //         r: 255 / 255,
+    //         g: 255 / 255,
+    //         b: 255 / 255,
+    //     });
+    //     GSAP.to(this.sunLight, {
+    //         intensity: 3,
+    //     });
+    //     GSAP.to(this.ambientLight, {
+    //         intensity: 1,
+    //     });
+    // }
 }
     resize() {
 
