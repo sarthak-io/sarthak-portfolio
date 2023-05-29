d043b318886c00253dd738444eddbd98a04b0f4d
sarthak-io <sarthak25ic049@satiengg.in>
2023-05-29 18:03:40 +0530

    updated 2.0

--- Experience/Utils/assets.js
+++ Experience/Utils/assets.js
@@ -2,7 +2,7 @@ export default [
     {
         name:"room",
         type:"glbModel",
-        path:"/textures/Portfolio 3d.glb"
+        path:"/textures/untitled.glb"
     },
     {
         name:"screen",
--- Experience/World/Environment.js
+++ Experience/World/Environment.js
@@ -14,22 +14,22 @@ export default class Environment {
         
     }
    setSunlight(){
-   this.sunLight = new THREE.DirectionalLight("#ffffff",3);
-   this.sunLight.castShadow= true;
-   this.sunLight.shadow.camera.far=10;
-   this.sunLight.shadow.mapSize.set(2048,2048);
-   this.sunLight.shadow.normalBias=0.05;
+//    this.sunLight = new THREE.DirectionalLight("#ffffff",3);
+//    this.sunLight.castShadow= true;
+//    this.sunLight.shadow.camera.far=10;
+//    this.sunLight.shadow.mapSize.set(2048,2048);
+//    this.sunLight.shadow.normalBias=0.05;
    
-   this.sunLight.position.set(-.5,3,1);
-   this.scene.add(this.sunLight)
-   this.ambientLight = new THREE.AmbientLight("#ffffff",1.5)
-   this.scene.add(this.ambientLight)
-   if(this.experience.sizes.device === "mobile"){
-    this.sunLight.shadow.mapSize.set(0,0);
-    this.sunLight.shadow.normalBias=0;
-    this.sunLight.intensity = 0.5; 
-    this.ambientLight.intensity = 0; 
-   }
+//    this.sunLight.position.set(-.5,3,1);
+//    this.scene.add(this.sunLight)
+//    this.ambientLight = new THREE.AmbientLight("#ffffff",1.5)
+//    this.scene.add(this.ambientLight)
+//    if(this.experience.sizes.device === "mobile"){
+//     this.sunLight.shadow.mapSize.set(0,0);
+//     this.sunLight.shadow.normalBias=0;
+//     this.sunLight.intensity = 0.5; 
+//     this.ambientLight.intensity = 0; 
+//    }
    }
    
    switchTheme(theme) {
